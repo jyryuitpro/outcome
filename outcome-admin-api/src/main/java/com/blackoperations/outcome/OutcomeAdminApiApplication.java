43fcab6a67a8cec541c36a689b48758d0eefb2ed
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-11-21 16:22:01 +0900

    29. 프로젝트 분리 - 1

--- outcome-admin-api/src/main/java/com/blackoperations/outcome/OutcomeAdminApiApplication.java
+++ outcome-admin-api/src/main/java/com/blackoperations/outcome/OutcomeAdminApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OutcomeApplication {
+public class OutcomeAdminApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(OutcomeApplication.class, args);
+        SpringApplication.run(OutcomeAdminApiApplication.class, args);
     }
 
 }
