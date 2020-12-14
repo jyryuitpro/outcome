e6db164e5df02a632e94a750378339767bda7568
jyryuitpro <jy.ryu.itpro@gmail.com>
2020-12-14 20:58:17 +0900

    54. 인가 - 1

--- outcome-common/src/main/java/com/blackoperations/outcome/utils/JwtUtil.java
+++ outcome-common/src/main/java/com/blackoperations/outcome/utils/JwtUtil.java
@@ -1,5 +1,6 @@
 package com.blackoperations.outcome.utils;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -22,5 +23,9 @@ public class JwtUtil {
                 .compact();
 
         return token;
+    }
+
+    public Claims getClaims(String token) {
+        return Jwts.parser().setSigningKey(key).parseClaimsJws(token).getBody();
     }
 }
